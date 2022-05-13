c2a4a37cbd4467f589ecf445f3b3188b5a6e2c30
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-05-13 03:46:39 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,28 @@
+function check(){
+    var c=0;
+    
+    var quiz1=document.question.question1.value;
+    var quiz2=document.question.question2.value;
+    var quiz3=document.question.question3.value;
+    var quiz4=document.question.question4.value;
+    var quiz5=document.question.question5.value;
+    var quiz6=document.question.question6.value;
+    var quiz7=document.question.question7.value;
+    var quiz8=document.question.question8.value;
+    var quiz9=document.question.question9.value;
+    var quiz10=document.question.question10.value;
+
+    if(q1=="Must not contain spaces ") {c++}
+    if(q2=="concat()method") {c++}
+    if(q3=="Camelcase") {c++}
+    if(q4=="declare") {c++}
+    if(q5=="math.ceil") {c++}
+    if(q6=="math.PI") {c++}
+    if(q7=="Global scopes") {c++}
+    if(q8=="function") {c++}
+    if(q9=="YES") {c++}
+    if(q10=="function") {c++}
+
+    document.write(c)
+    
+}
