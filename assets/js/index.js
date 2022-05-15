c297f78bc4202ce321576eaa3d74f32a69009758
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-05-15 19:40:59 +0300

    adds score.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,29 @@
-
-function check(){
-  var c=0;
+let answers = [
   
-  var quiz1=document.question.question1.value;
-  var quiz2=document.question.question2.value;
-  var quiz3=document.question.question3.value;
-  var quiz4=document.question.question4.value;
-  var quiz5=document.question.question5.value;
-  var quiz6=document.question.question6.value;
-  var quiz7=document.question.question7.value;
-  var quiz8=document.question.question8.value;
-  var quiz9=document.question.question9.value;
-  var quiz10=document.question.question10.value;
+  "Must not contain spaces",
+  "concat()method",
+  "Camelcase",
+  "declare",
+  "math.ceil",
+  "math.PI",
+  "Global scopes",
+  "loops",
+  "YES",
+  "While",
+];
+let form=document.querySelector('form')
 
-  if(q1=="Must not contain spaces ") {c++}
-  if(q2=="concat()method") {c++}
-  if(q3=="Camelcase") {c++}
-  if(q4=="declare") {c++}
-  if(q5=="math.ceil") {c++}
-  if(q6=="math.PI") {c++}
-  if(q7=="Global scopes") {c++}
-  if(q8=="function") {c++}
-  if(q9=="YES") {c++}
-  if(q10=="function") {c++}
-console.log(quiz1)
-    document.write(c)
-  
-}
-check()
+form.addEventListener("submit", function(e) {
+  let score=0;
+  e.preventDefault();
+  var data = new FormData(form);
+  let i=0
+  for (const [name,value] of data) {
+    if (answers[i]==value){
+      score++
+    }
+    i++
+   
+  }
+  alert("your score is "+score)
+})
