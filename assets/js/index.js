d5a2dd8bdc05c876c0d050c0508bb58151b22350
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-05-16 10:52:31 +0300

    adds lines of code to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,25 @@
-let answers = [
-  
-  "Must not contain spaces",
-  "concat()method",
-  "Camelcase",
-  "declare",
-  "math.ceil",
-  "math.PI",
-  "Global scopes",
-  "loops",
-  "YES",
-  "While",
-];
-let form=document.querySelector('form')
+let answers = {
+  "quiz1": "Must not contain spaces",
+  "quiz2": "concat()method",
+  "quiz3": "Camelcase",
+  "quiz4": "declare",
+  "quiz5": "math.ceil",
+  "quiz6": "math.PI",
+  "quiz7": "Global scopes",
+  "quiz8": "loops",
+  "quiz9": "YES",
+  "quiz10": "While",
+};
+let form = document.querySelector('form')
 
-form.addEventListener("submit", function(e) {
-  let score=0;
+form.addEventListener("submit", function (e) {
+  let score = 0;
   e.preventDefault();
   var data = new FormData(form);
-  let i=0
-  for (const [name,value] of data) {
-    if (answers[i]==value){
+  for (const [name, value] of data) {
+    if (answers[name] == value) {
       score++
     }
-    i++
-   
   }
   document.write(`Your score is ${score*10}%`)
-  
 })
